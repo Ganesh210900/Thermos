bcdcba7b26ca7ee14281569dd4a1a734cd7c8afa
ganes <ganes@DESKTOP-G9FBDHJ>
2022-06-12 10:59:19 +0530

    commitS2

--- SEE_exam/src/Thermos.java
+++ SEE_exam/src/Thermos.java
@@ -3,30 +3,43 @@ public class Thermos {
 	String brand ;
 	double liquid;
 	String liq_name;
+	double liq;
+	double liq2;
 	
-	Thermos(String brand,double liquid,String liq_name)
+	Thermos(String brand,double liquid,String liq_name,double liq,double liq2)
 	{
 		this.brand=brand;
 		this.liquid=liquid;
 		this.liq_name=liq_name;
+		this.liq=liq;
+		this.liq2=liq2;
 	}
 	
-	void addLiquid(double liq)
+	double addLiquid()
 	{
 		liquid=liquid+liq; 
+		return liquid;
 	}
-	
+	double PourLiq()
+	{
+		liquid = liquid-liq2;
+		return liquid;
+	}
 	void display()
 	{
 		System.out.println("Brand: "+brand);
 		System.out.println("Liquid name:"+liq_name);
-		System.out.println("Liquid: "+liquid+" litrs");
+		System.out.println("Liquid: "+liq+" litrs");
+		System.out.println("After poured : "+liq2+" litrs");
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Thermos u1=new Thermos("Godrez",0, "Orange Juice");
-		u1.addLiquid(0.5);
+		Thermos u1=new Thermos("Godrez",0, "Orange Juice",0.5,0.25);
+		u1.addLiquid();
+		u1.PourLiq();
 		u1.display();
+		
 
 	}
 
